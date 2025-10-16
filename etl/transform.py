5d1566a692e4c832c489ff54b087df9ff6d1c6e6
Dani <94472881+suri0702@users.noreply.github.com>
2025-10-16 08:58:03 -0300

    Update transform.py
    Substituindo valores nulos por 'Não Informado'

--- etl/transform.py
+++ etl/transform.py
@@ -1,1 +1,18 @@
+--2. Transformação (T)
+
+---Padronizar a coluna de data para o formato YYYY-MM-DD. (Alessa)
+
+---Substituir valores nulos por "Não informado". (Dani)
+
+import pandas as pd
+
+dataframe = pd.read_csv('vendas.csv')
+
+newDataframe = dataframe.fillna('Não Informado')
+
+print(newDataframe.to_string())
+
+---Corrigir a quantidade para ser sempre um número inteiro. (Vitoria)
+
+---Remover ou corrigir preços negativos. (Nicoli)
 

3cc81528a737e2795a5691e62954db7e926bfc5d
Dani <94472881+suri0702@users.noreply.github.com>
2025-10-16 08:17:34 -0300

    Update load.py
    Criando o database vendas.db

--- etl/load.py
+++ etl/load.py
@@ -1,1 +1,12 @@
+--3. Carga (L)
+
+
+---Criar um banco SQLite (arquivo vendas.db). (Dani)
+
+import sqlite3 as sql
+conexao = sql.connect('vendas.db')
+print("Banco de dados criado com sucesso!")
+
+
+---Salvar os dados tratados em uma tabela chamada tb_vendas. (Michelle) (Andrea)
 

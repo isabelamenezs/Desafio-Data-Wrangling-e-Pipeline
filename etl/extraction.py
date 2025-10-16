248bdd6c83005cde40a985c3f2012c8a671f56eb
Dani <94472881+suri0702@users.noreply.github.com>
2025-10-16 17:06:05 -0300

    Update extraction.py

--- etl/extraction.py
+++ etl/extraction.py
@@ -1,1 +1,27 @@
+--1. Leitura e Exploração Inicial
+
+---1.	Leia o arquivo CSV e visualize as 5 primeiras linhas do dataset. (michelle) (isabela)
+import pandas as pd
+dataframe = pd.read_csv('vendas_raw.csv')
+print(dataframe.head())
+
+---2.	Verifique o número de linhas e colunas. (andrea)
+contColuna = len(dataframe.axes[0])
+contLinha = len(dataframe.axes[1]) 
+print("Número de colunas:"+str(contColuna))
+print("Número de linhas:"+str(contLinha))
+
+---3.	Descubra o tipo de dados (dtype) de cada coluna. (alana)
+
+---4.	Conte os valores ausentes existentes em cada coluna. (ana)
+
+
+
+
+--2. Seleção e Filtragem de Dados
+
+---1.	Filtre apenas as linhas onde o preço unitário é maior que 100. (Isabela)
+
+---2.	Ordene o dataset pelo valor do preço em ordem decrescente. (Amanda) (Alessa)
+
 

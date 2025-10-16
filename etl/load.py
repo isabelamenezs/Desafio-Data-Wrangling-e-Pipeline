0ea8ed93931e1e124c76f96228085bf3487d178f
Alana Sampaio <alana.sampaio2000@gmail.com>
2025-10-16 14:48:24 -0300

    Desafio extra, criação da tabela tb_clientes

--- etl/load.py
+++ etl/load.py
@@ -10,3 +10,13 @@ print("Banco de dados criado com sucesso!")
 
 ---Salvar os dados tratados em uma tabela chamada tb_vendas. (Michelle) (Andrea)
 
+
+
+
+--Desafio Extra:
+---Criar uma segunda tabela tb_clientes contendo apenas clientes únicos. (Alana)
+
+tabela_vendas = sqlite3.connect("vendas.db")
+df_clientes = df_vendas[["cliente"]].drop_duplicates().reset_index(drop=True)
+df_clientes.to_sql("tb_clientes", tabela_vendas, if_exists="replace", index=False)
+tabela_vendas.close()

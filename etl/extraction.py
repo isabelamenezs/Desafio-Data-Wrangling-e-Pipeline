1f02a7cbe423eb6246f208eb84a47b36b39b7812
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-20 16:47:32 -0300

    Update extraction.py
    Filtro dos precos_unitarios maiores que 100

--- etl/extraction.py
+++ etl/extraction.py
@@ -1,31 +1,34 @@
---1. Leitura e Exploração Inicial
+#1. Leitura e Exploração Inicial
 
----1.	Leia o arquivo CSV e visualize as 5 primeiras linhas do dataset. (michelle) (isabela)
+#1.	Leia o arquivo CSV e visualize as 5 primeiras linhas do dataset. (michelle) (isabela)
 import pandas as pd
 dataframe = pd.read_csv('vendas_raw.csv')
 print(dataframe.head())
 
----2.	Verifique o número de linhas e colunas. (andrea)
+#2.	Verifique o número de linhas e colunas. (andrea)
 contColuna = len(dataframe.axes[0])
 contLinha = len(dataframe.axes[1]) 
 print("Número de colunas:"+str(contColuna))
 print("Número de linhas:"+str(contLinha))
 
----3.	Descubra o tipo de dados (dtype) de cada coluna. (alana)
+#3.	Descubra o tipo de dados (dtype) de cada coluna. (alana)
 
 tipo_colunas = dataframe.dtypes
 print(tipo_colunas)
 
----4.	Conte os valores ausentes existentes em cada coluna. (ana)
+#4.	Conte os valores ausentes existentes em cada coluna. (ana)
 
 
 
 
---2. Seleção e Filtragem de Dados
+#2. Seleção e Filtragem de Dados
 
----1.	Filtre apenas as linhas onde o preço unitário é maior que 100. (Isabela)
+#1.	Filtre apenas as linhas onde o preço unitário é maior que 100. (Isabela)
 
----2.	Ordene o dataset pelo valor do preço em ordem decrescente. (Amanda) (Alessa)
+preco_maior_que_100 = df['preco_unitario'] > 100
+print(dataframe[preco_maior_que_100])
+
+#2.	Ordene o dataset pelo valor do preço em ordem decrescente. (Amanda) (Alessa)
 import pandas as pd
 
 # Leitura do dataset

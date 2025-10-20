1db7c2c11b4988ff82c167ebe6ff39a35d0a5351
Alana Sampaio <alana.sampaio2000@gmail.com>
2025-10-20 20:23:24 -0300

    atualização das questões e ajuste de variáveis

--- etl/load.py
+++ etl/load.py
@@ -10,14 +10,16 @@ print("Banco de dados criado com sucesso!")
 
 # Salvar os dados tratados em uma tabela tb_vendas. (Michelle) (Andrea)
 
+dataframe.to_sql("tb_vendas", conexao, if_exists="replace", index=False)
 
+conexao.close()
 
 
 ## Desafio Extra:
 # Criar uma segunda tabela tb_clientes contendo apenas clientes únicos. (Alana)
 
 conexao = sqlite3.connect("vendas.db")
-df_clientes = df_vendas[["cliente"]].drop_duplicates().reset_index(drop=True)
+df_clientes = dataframe[["cliente"]].drop_duplicates().reset_index(drop=True)
 df_clientes.to_sql("tb_clientes", conexao, if_exists="replace", index=False)
 conexao.close()
 

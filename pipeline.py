26271c708a20be14cf47b9872549117eee323ec2
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-20 22:07:13 -0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,1 +1,17 @@
+from etl.extraction import extract_data
+from etl.transform import transform_data
+from etl.load import load_data
+
+def run_pipeline():
+    # Caminhos
+    input_path = 'data/raw/vendas.csv'
+    output_path = ''
+
+    # Etapas ETL
+    df = extract_data(input_path)
+    df = transform_data(df)
+    load_data(df, output_path)
+
+if __name__ == "__main__":
+    run_pipeline()
 

964d6a441d645d77dc7160dd0060ae3421f21682
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-20 22:02:27 -0300

    Update transform.py

--- etl/transform.py
+++ etl/transform.py
@@ -1,35 +1,35 @@
 # 2. Transformação (T)
+def transform_data(df):
+    # Padronizar a coluna de data (YYYY-MM-DD). (Alessa)
+    # Padronizar coluna de data
+    df['data'] = pd.to_datetime(df['data'], errors='coerce').dt.strftime('%Y-%m-%d')
+    
+    # Verificar o resultado
+    print(df['data'].head())
 
-# Padronizar a coluna de data (YYYY-MM-DD). (Alessa)
-# Padronizar coluna de data
-df['data'] = pd.to_datetime(df['data'], errors='coerce').dt.strftime('%Y-%m-%d')
+    # Substituir valores nulos por "Não informado". (Dani)
 
-# Verificar o resultado
-print(df['data'].head())
+    import pandas as pd
 
-# Substituir valores nulos por "Não informado". (Dani)
+    dataframe = pd.read_csv('vendas.csv')
 
-import pandas as pd
+    newDataframe = dataframe.fillna('Não Informado')
 
-dataframe = pd.read_csv('vendas.csv')
+    print(newDataframe.to_string())
 
-newDataframe = dataframe.fillna('Não Informado')
+    # Corrigir quantidade para ser sempre um número inteiro. (Vitória)
 
-print(newDataframe.to_string())
+    dataframe['quantidade'].unique()
 
-# Corrigir quantidade para ser sempre um número inteiro. (Vitória)
+    dataframe['quantidade'] = dataframe['quantidade'].replace('três', '3')
 
-dataframe['quantidade'].unique()
+    dataframe['quantidade'] = pd.to_numeric(dataframe['quantidade'])
+    dataframe.dtypes
 
-dataframe['quantidade'] = dataframe['quantidade'].replace('três', '3')
+    # Remover ou corrigir preços negativos.
+    if 'preco_unitario' in dataframe.columns:
+        dataframe = dataframe[dataframe['preco_unitario'] >= 0]
 
-dataframe['quantidade'] = pd.to_numeric(dataframe['quantidade'])
-dataframe.dtypes
-
-# Remover ou corrigir preços negativos.
-if 'preco_unitario' in dataframe.columns:
-    dataframe = dataframe[dataframe['preco_unitario'] >= 0]
-
-# Criar uma nova coluna valor_total = quantidade * preco_unitario.
-if 'quantidade' in df.columns and 'preco_unitario' in dataframe.columns:
-  dataframe['valor_total'] = dataframe['quantidade'] * dataframe['preco_unitario']
+    # Criar uma nova coluna valor_total = quantidade * preco_unitario.
+    if 'quantidade' in df.columns and 'preco_unitario' in dataframe.columns:
+        dataframe['valor_total'] = dataframe['quantidade'] * dataframe['preco_unitario']

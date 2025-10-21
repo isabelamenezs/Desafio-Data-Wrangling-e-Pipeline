97b114acd8c1a6e13572fa62d806c035ee1b3aab
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-20 21:48:52 -0300

    Update extraction.py

--- etl/extraction.py
+++ etl/extraction.py
@@ -1,22 +1,22 @@
 #1. Leitura e Exploração Inicial
 
-#1.	Leia o arquivo CSV e visualize as 5 primeiras linhas do dataset. (michelle) (isabela)
+#1.	Leia o arquivo CSV e visualize as 5 primeiras linhas do dataset.
 import pandas as pd
 dataframe = pd.read_csv('vendas_raw.csv')
-print(dataframe.head())
+print(dataframe.head(5))
 
-#2.	Verifique o número de linhas e colunas. (andrea)
+#2.	Verifique o número de linhas e colunas.
 contColuna = len(dataframe.axes[0])
 contLinha = len(dataframe.axes[1]) 
 print("Número de colunas:"+str(contColuna))
 print("Número de linhas:"+str(contLinha))
 
-#3.	Descubra o tipo de dados (dtype) de cada coluna. (alana)
+#3.	Descubra o tipo de dados (dtype) de cada coluna.
 
 tipo_colunas = dataframe.dtypes
 print(tipo_colunas)
 
-#4.	Conte os valores ausentes existentes em cada coluna. (ana)
+#4.	Conte os valores ausentes existentes em cada coluna.
 
 
 
@@ -28,17 +28,12 @@ print(tipo_colunas)
 preco_maior_que_100 = dataframe['preco_unitario'] > 100
 print(dataframe[preco_maior_que_100])
 
-#2.	Ordene o dataset pelo valor do preço em ordem decrescente. (Amanda) (Alessa)
-import pandas as pd
+#2.	Ordene o dataset pelo valor do preço em ordem decrescente. 
 
-# Leitura do dataset
-df = pd.read_csv('2lBCPRISD6UzKRFg5VZ1_vendas (3).csv')
-
-# Ordenar pelo preço unitário em ordem decrescente
-df_ordenado = df.sort_values(by='preco_unitario', ascending=False)
+df_ordenado = dataframe.sort_values(by='preco_unitario', ascending=False)
 
 # Exibir as 5 primeiras linhas para verificação
-print(df_ordenado.head())
+print(df_ordenado.head(5))
 
 
 

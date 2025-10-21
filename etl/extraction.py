05c88d2ea712a445e770c35d121a544aa7a8b5b4
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-20 21:59:52 -0300

    Update extraction.py

--- etl/extraction.py
+++ etl/extraction.py
@@ -1,39 +1,36 @@
 #1. Leitura e Exploração Inicial
-
-#1.	Leia o arquivo CSV e visualize as 5 primeiras linhas do dataset.
 import pandas as pd
-dataframe = pd.read_csv('vendas_raw.csv')
-print(dataframe.head(5))
+def explore_data(filepath):
+  #1.	Leia o arquivo CSV e visualize as 5 primeiras linhas do dataset.
+  dataframe = pd.read_csv(filepath)
+  print(dataframe.head(5))
+  
+  #2.	Verifique o número de linhas e colunas.
+  contColuna = len(dataframe.axes[0])
+  contLinha = len(dataframe.axes[1]) 
+  print("Número de colunas:"+str(contColuna))
+  print("Número de linhas:"+str(contLinha))
 
-#2.	Verifique o número de linhas e colunas.
-contColuna = len(dataframe.axes[0])
-contLinha = len(dataframe.axes[1]) 
-print("Número de colunas:"+str(contColuna))
-print("Número de linhas:"+str(contLinha))
+  #3.	Descubra o tipo de dados (dtype) de cada coluna.
+  tipo_colunas = dataframe.dtypes
+  print(tipo_colunas)
 
-#3.	Descubra o tipo de dados (dtype) de cada coluna.
-
-tipo_colunas = dataframe.dtypes
-print(tipo_colunas)
-
-#4.	Conte os valores ausentes existentes em cada coluna.
+  #4.	Conte os valores ausentes existentes em cada coluna.
 
 
 
 
-#2. Seleção e Filtragem de Dados
+  #2. Seleção e Filtragem de Dados
 
-#1.	Filtre apenas as linhas onde o preço unitário é maior que 100. (Isabela)
+  #1.	Filtre apenas as linhas onde o preço unitário é maior que 100.
+  preco_maior_que_100 = dataframe['preco_unitario'] > 100
+  print(dataframe[preco_maior_que_100])
 
-preco_maior_que_100 = dataframe['preco_unitario'] > 100
-print(dataframe[preco_maior_que_100])
+  #2.	Ordene o dataset pelo valor do preço em ordem decrescente. 
+  df_ordenado = dataframe.sort_values(by='preco_unitario', ascending=False)
 
-#2.	Ordene o dataset pelo valor do preço em ordem decrescente. 
-
-df_ordenado = dataframe.sort_values(by='preco_unitario', ascending=False)
-
-# Exibir as 5 primeiras linhas para verificação
-print(df_ordenado.head(5))
+  # Exibir as 5 primeiras linhas para verificação
+  print(df_ordenado.head(5))
 
 
 

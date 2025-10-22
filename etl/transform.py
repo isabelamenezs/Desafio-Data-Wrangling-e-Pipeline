517d7e4520789571a2e04aa262cefb2583ffe537
Alana Sampaio <alana.sampaio2000@gmail.com>
2025-10-22 16:37:24 -0300

    Update transform.py

--- etl/transform.py
+++ etl/transform.py
@@ -1,18 +1,28 @@
+import pandas as pd
+
 # 2. Transformação (T)
-def transform_data(df):
+def transform_data(filepath):
+
+    dataframe = pd.read_csv(filepath)
+
     # Padronizar a coluna de data (YYYY-MM-DD). (Alessa)
-    # Padronizar coluna de data
-    df['data'] = pd.to_datetime(df['data'], errors='coerce').dt.strftime('%Y-%m-%d')
-    
-    # Verificar o resultado
-    print(df['data'].head())
+    dataframe['data_venda'] = (
+        dataframe['data_venda']
+        .astype(str)
+        .str.strip()
+        .str.replace(r'[/\.]', '-', regex=True)
+    )
+
+    datas = pd.to_datetime(dataframe['data_venda'], errors='coerce', format='%Y-%m-%d')
+
+    mask_nat = datas.isna()
+    datas[mask_nat] = pd.to_datetime(dataframe.loc[mask_nat, 'data_venda'], errors='coerce', dayfirst=True)
+
+    dataframe['data_venda'] = datas.dt.strftime('%Y-%m-%d')
+
+    print(dataframe['data_venda'].head(20))
 
     # Substituir valores nulos por "Não informado". (Dani)
-
-    import pandas as pd
-
-    dataframe = pd.read_csv('vendas.csv')
-
     newDataframe = dataframe.fillna('Não Informado')
 
     print(newDataframe.to_string())
@@ -26,10 +36,12 @@ def transform_data(df):
     dataframe['quantidade'] = pd.to_numeric(dataframe['quantidade'])
     dataframe.dtypes
 
-    # Remover ou corrigir preços negativos.
+    # Remover ou corrigir preços negativos. (Nicoli)
     if 'preco_unitario' in dataframe.columns:
         dataframe = dataframe[dataframe['preco_unitario'] >= 0]
 
-    # Criar uma nova coluna valor_total = quantidade * preco_unitario.
+    # Criar uma nova coluna valor_total = quantidade * preco_unitario. (Nicoli)
     if 'quantidade' in df.columns and 'preco_unitario' in dataframe.columns:
         dataframe['valor_total'] = dataframe['quantidade'] * dataframe['preco_unitario']
+
+    return dataframe

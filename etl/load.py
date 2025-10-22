b99d00321836c0a9973e4b4b3fa0125dbdf24936
Ana Beatriz Costa <66637302+AnaBeCosta@users.noreply.github.com>
2025-10-22 20:36:17 -0300

    Update load.py

--- etl/load.py
+++ etl/load.py
@@ -1,30 +1,37 @@
 # 3. Carga (L)
 def load_data(df, output_path):
+  
   # Criar um banco SQLite (arquivo vendas.db). (Dani)
   import sqlite3 as sql
   conexao = sql.connect('vendas.db')
   print("Banco de dados criado com sucesso!")
 
-
   # Salvar os dados tratados em uma tabela tb_vendas. (Michelle) (Andrea)
-
   dataframe.to_sql("tb_vendas", conexao, if_exists="replace", index=False)
-
   conexao.close()
-
 
   ## Desafio Extra:
   # Criar uma segunda tabela tb_clientes contendo apenas clientes únicos. (Alana)
-
   conexao = sqlite3.connect("vendas.db")
   df_clientes = dataframe[["cliente"]].drop_duplicates().reset_index(drop=True)
   df_clientes.to_sql("tb_clientes", conexao, if_exists="replace", index=False)
-  conexao.close()
 
   # Relacionar tb_vendas com tb_clientes via chave estrangeira. (Ana)
-
+  dataframe = dataframe.merge(df_clientes, on="cliente", how="left")
+  
   # Escrever uma consulta SQL que mostre: total de vendas por categoria. (Vitória)
-
-  SELECT categoria, SUM(valor_total) AS total_vendas
-  FROM tb_vendas
-  GROUP BY categoria;
+  query = """
+          SELECT
+              categoria,
+              SUM(valor_total) AS total_vendas
+          FROM tb_vendas
+          GROUP BY categoria;
+          """
+  
+  df_resultado = pd.read_sql_query(query, conexao)
+  
+  # Exibir resultado
+  print("📊 Total de vendas por categoria:\n")
+  display(df_resultado)
+  
+  conexao.close()
